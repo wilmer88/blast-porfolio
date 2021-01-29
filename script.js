0b49cb4fe27984e9daa82de87cfc345bef345bfc
wilmerbaby <wilmerbaby@gmail.com>
2021-01-29 03:43:44 -0500

    added modal

---
+++ script.js
@@ -0,0 +1,28 @@
+var man = document.querySelector("intro");
+
+var cuerpo = document.querySelector("body");
+var soon = document.querySelector("son");
+
+
+console.log(cuerpo);
+
+
+
+
+
+
+$(window).on('load', function() {
+   
+    $('#intro').modal('show');
+});
+
+
+
+  
+    
+
+
+
+
+
+
