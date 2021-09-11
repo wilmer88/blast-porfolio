dfa27653a2696837b650cd2291154000cb199377
wilmerbaby <wilmerbaby@gmail.com>
2021-09-11 12:27:01 -0400

    change a error in having a pronoun

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-var man = document.querySelector("intro");
-
-var cuerpo = document.querySelector("body");
-var soon = document.querySelector("son");
-
-
-console.log(cuerpo);
-
-
-
-
-
-
-$(window).on('load', function() {
-   
-    $('#intro').modal('show');
-});
-
-
-
-  
-    
-
-
-
-
-
-
+// $(document).ready(function () {
+//   // $(window).on('load', function() {
+//   //     $('#intro').modal('show');
+//   // });
+// });
