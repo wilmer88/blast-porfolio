192d76153549fb406a74ff48adc6ca44d90ce068
Wilmer Rivera <wilmerbaby@gmail.com>
2023-01-16 18:23:07 -0500

    fixed footer responsiveness

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 
-  $(window).on('load', function() {
+//   $(window).on('load', function() {
    
-    $('#intro').modal('show');
+//     $('#intro').modal('show');
+    
+//   const Red = -600;
+
+//   if(Red < -1000){
+//     return console.log("red is more favorite than -1000 and gets: " + 20 + " points")
+//   }
+// else if(Red < -800){
+//   return console.log("red is more favorite than -800 and gets: " + 15 + " points")
+// }
+// else if(Red < -500){
+//   return console.log("red is more favorite than -500 and gets: " + 10 + " points")
+// }
    
+//   });
 
 
-      
 
 
-   
-  });
+  
+  // function fizzBuck(numVar)
+  // {
+  //   for(let i = 1; i <= numVar; i++ )
+  //   {
+  //     if(i % 3 == 0 && i % 5 == 0)
+  //     {
+  //       console.log("fizz buzz")
+  //     }
+  //     else if(i % 3 == 0)
+  //     { 
+  //       console.log("fizz")
+  //     }
+  //     else if(i % 5 == 0)
+  //     { 
+  //       console.log("buzzz")
+  //     }
+    
+  //     else{console.log(i)}
+  //   }
+  // }
+  
