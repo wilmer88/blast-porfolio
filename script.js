28240d372d75531e2ef0c20b74214edf641df4c7
Wilmer Rivera <wilmerbaby@gmail.com>
2023-01-19 04:46:38 -0500

    added model logic to use local storage to only show model every other page reload

--- script.js
+++ script.js
@@ -3,7 +3,50 @@
    
 //     $('#intro').modal('show');
     
-//   const Red = -600;
+ 
+   
+//   });
+
+// $(document).ready(function() {
+//     var dialogShown = localStorage.getItem('wilmeModel')
+    
+//     if (!dialogShown) {
+//         $(window).on("load",function(){
+//             $('#intro').modal('show');
+//             $.cookie('wilmeModel', 1)
+//         });
+//     }
+//     });
+
+
+$(document).ready(function() {
+  var dialogShown = localStorage.getItem('wilmeModel')
+  
+  if (!dialogShown || dialogShown == 0) {
+      $(window).on("load",function(){
+          $('#intro').modal('show');
+          localStorage.setItem('wilmeModel', 1)
+      });
+  }else if(dialogShown == 1){
+    localStorage.setItem("wilmeModel", 0)
+  }
+  }
+  
+  );
+
+//   $(document).ready(function() {
+//     var dialogShown = $.cookie('dialogShown');
+    
+//     if (!dialogShown) {
+//         $(window).load(function(){
+//             $('#wizardmodal').modal('show');
+//             $.cookie('dialogShown', 1);
+//         });
+//     }
+//     });
+
+
+//  const Red = -600;
 
 //   if(Red < -1000){
 //     return console.log("red is more favorite than -1000 and gets: " + 20 + " points")
@@ -14,11 +57,6 @@
 // else if(Red < -500){
 //   return console.log("red is more favorite than -500 and gets: " + 10 + " points")
 // }
-   
-//   });
-
-
-
 
   
   // function fizzBuck(numVar)
